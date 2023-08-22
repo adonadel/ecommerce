1b747e124f565807a5188c0da8d208b3d1e674fe
ALEXANDRE DONADEL FERNANDES <alexandre.fernandes1@alunos.sc.senac.br>
2023-08-22 19:15:47 -0300

    primeiro commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+
+  },
+  {
+    path: 'categoria',
+    component: CategoriaComponent,
+    children: [
+      {
+        path: '',
+        component: CategoriaListarComponent
+      },
+      {
+        path: 'listar',
+        component: CategoriaListarComponent
+      },
+      {
+        path: 'adicionar',
+        component: CategoriaFormComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    CategoriaComponent,
+    CategoriaListarComponent,
+    CategoriaFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
